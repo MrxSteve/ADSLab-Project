1e7b56654cd9729cdcbdffa278595462a7208261
jarlexdev <jarlexdev@gmail.com>
2025-04-11 15:42:52 -0600

    chore: configurar Flask y registrar blueprint de libros

--- src/app.py
+++ src/app.py
@@ -1,25 +1,38 @@
 from flask import Flask
 from flask_cors import CORS
 from config.config import app_config
+from apis.libros.routes import Libro
 
+# Crear una instancia de la aplicación Flask
 app = Flask(__name__)
 
+# Habilitar CORS para todas las rutas y orígenes
+# Esto permite que el frontend realice solicitudes a la API
 CORS(app)
 
-def pageNotFound(e):
-    return "Page not found", 404
 
-def internalServerError(e):
-    return "Internal server error", 500
+# Definir un manejador de errores para rutas no encontradas
+def pagina_no_encontrada(error):
+    return "<h1>Página no encontrada</h1>", 404
 
-@app.route('/')
+
+def error_servidor(error):
+    return "<h1>Error interno del servidor</h1>", 500
+
+
+@app.route("/")
 def principal():
-    return "Welcome to the Flask API!"
+    return "<h1>Bienvenido a mi aplicación con Flask</h1>"
 
-if __name__ == '__main__':
-    app.config.from_object(app_config['development'])
 
-    app.register_error_handler(404, pageNotFound)
-    app.register_error_handler(500, internalServerError)
+if __name__ == "__main__":
+    # Configurar la aplicación Flask con la configuración de desarrollo
+    app.config.from_object(app_config["development"])
+    # apartado para rutas
+    app.register_blueprint(Libro.main, url_prefix="/api/libros")
+    # Registrar los manejadores de errores
+    app.register_error_handler(404, pagina_no_encontrada)
+    app.register_error_handler(500, error_servidor)
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Iniciamos el servidor de Flask, escuchando en el puerto 5000
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- src/config/config.py
+++ src/config/config.py
@@ -1,11 +1,18 @@
 from decouple import config
 
+
+# Define la clase secreta desde el archivo .env o variable de entorno
 class Config:
-    SECRET_KEY = config('SECRET_KEY')
+    SECRET_KEY = config("SECRET_KEY")
+
+    # Activa el modo de depuración para el entorno de desarrollo
+
 
 class DevelopmentConfig(Config):
-    DEBUG: True
+    DEBUG = True
 
+
+# Diccionario para mapear nombres de entornos a sus configuraciones
 app_config = {
-    'development': DevelopmentConfig
+    "development": DevelopmentConfig,
 }
